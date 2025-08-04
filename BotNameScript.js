187c383e8952701fbae988fa601919f581209642
alien-sunset <alien_sunset@yahoo.com>
2025-08-04 01:22:27 -0400

    Initial Commit

---
+++ BotNameScript.js
@@ -0,0 +1,19 @@
+//this is the js script that makes the bot run. you shouldn't need to change anything here 
+
+//this tells the script to run right away when your pagtet loads
+window.addEventListener("load", () => {
+
+	//this is the typical tracery setup code
+	var grammar = tracery.createGrammar(BotTracery);
+	grammar.addModifiers(tracery.baseEngModifiers);
+
+	//this function runs the tracery and inserts the output into the webpage
+	function BotOutput() {
+		var result = grammar.flatten("#origin#");
+		document.getElementById("output").innerHTML = result;
+	}
+
+	BotOutput(); //this runs the function and outputs the first output
+
+	document.getElementById("new").addEventListener("click", BotOutput); //this makes a new output when the button is clicked
+});
